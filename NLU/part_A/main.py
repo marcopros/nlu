6106c735d45a57d0000aec8f3996275ff5dad1d5
marcopros <marco.prosperi3@gmail.com>
2025-04-12 12:07:11 +0200

    Refactor training loop to support multiple runs; update report with new metrics

--- NLU/part_A/main.py
+++ NLU/part_A/main.py
@@ -90,70 +90,65 @@ if __name__ == "__main__":
     hid_size = 200
     emb_size = 300
 
-    lr = 0.0001
-    clip = 5
+    lr = 0.0001 # learning rate
+    clip = 5 # Clip the gradient
+
 
     out_slot = len(lang.slot2id)
     out_int = len(lang.intent2id)
     vocab_len = len(lang.word2id)
 
-    model = ModelIAS(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN).to(device)
-    model.apply(init_weights)
-
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
-    criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
-
+    n_epochs = 200
+    runs = 5
+    
     # Train Loop
-    n_epochs = 50
-    patience = 3
-    losses_train = []
-    losses_dev = []
-    sampled_epochs = []
-    best_f1 = 0
-    for x in tqdm(range(1,n_epochs)):
-        loss = train_loop(train_loader, optimizer, criterion_slots, 
-                        criterion_intents, model, clip=clip)
-        if x % 5 == 0: # We check the performance every 5 epochs
-            sampled_epochs.append(x)
-            losses_train.append(np.asarray(loss).mean())
-            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
-                                                        criterion_intents, model, lang)
-            losses_dev.append(np.asarray(loss_dev).mean())
-            
-            f1 = results_dev['total']['f']
-            # For decreasing the patience you can also use the average between slot f1 and intent accuracy
-            if f1 > best_f1:
-                best_f1 = f1
-                # Here you should save the model
-                patience = 3
-            else:
-                patience -= 1
-            if patience <= 0: # Early stopping with patience
-                break # Not nice but it keeps the code clean
+    slot_f1s, intent_acc = [], []
+    for x in tqdm(range(0, runs)):
+        model = ModelIAS(hid_size, out_slot, out_int, emb_size, 
+                        vocab_len, pad_index=PAD_TOKEN).to(device)
+        model.apply(init_weights)
 
-    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
-                                            criterion_intents, model, lang)    
-    print('Slot F1: ', results_test['total']['f'])
-    print('Intent Accuracy:', intent_test['accuracy'])
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+        criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+        criterion_intents = nn.CrossEntropyLoss()
+        
+
+        patience = 3
+        losses_train = []
+        losses_dev = []
+        sampled_epochs = []
+        best_f1 = 0
+        for x in range(1,n_epochs):
+            loss = train_loop(train_loader, optimizer, criterion_slots, 
+                            criterion_intents, model)
+            if x % 5 == 0:
+                sampled_epochs.append(x)
+                losses_train.append(np.asarray(loss).mean())
+                results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                            criterion_intents, model, lang)
+                losses_dev.append(np.asarray(loss_dev).mean())
+                f1 = results_dev['total']['f']
+
+                if f1 > best_f1:
+                    best_f1 = f1
+                else:
+                    patience -= 1
+                if patience <= 0: # Early stopping with patient
+                    break # Not nice but it keeps the code clean
+
+        results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
+                                                criterion_intents, model, lang)
+        intent_acc.append(intent_test['accuracy'])
+        slot_f1s.append(results_test['total']['f'])
+    slot_f1s = np.asarray(slot_f1s)
+    intent_acc = np.asarray(intent_acc)
+    print('Slot F1', round(slot_f1s.mean(),3), '+-', round(slot_f1s.std(),3))
+    print('Intent Acc', round(intent_acc.mean(), 3), '+-', round(slot_f1s.std(), 3))
+
 
     folder_name = create_report_folder()
     generate_plots(sampled_epochs, losses_train, losses_dev, os.path.join(folder_name,"plot.png"))
 
-    # Assicurati che la directory 'bin' esista
-    # bin_path = os.path.join(path, "bin")
-    # os.makedirs(bin_path, exist_ok=True)
-    
-    # PATH = os.path.join(bin_path, "weights_1.pt")
-    # saving_object = {
-    #     "epoch": x, 
-    #     "model": model.state_dict(), 
-    #     "optimizer": optimizer.state_dict(), 
-    #     "w2id": lang.word2id, 
-    #     "slot2id": lang.slot2id, 
-    #     "intent2id": lang.intent2id
-    # }
-    # torch.save(saving_object, PATH)
 
     # Assicurati che la directory 'report/bin' esista
     report_bin_path = os.path.join(folder_name, "bin")
